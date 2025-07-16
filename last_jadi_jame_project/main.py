a054151ea3e1918e96775563d5b54f6c7aa1e36d
k0thar <k0tharkh0dabakhshi0@gmail.com>
2025-07-16 18:04:52 +0330

    Create main.py

---
+++ last_jadi_jame_project/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 15 17:24:35 2025
+
+@author: h
+"""
+
+from to_do_list.py import ToDoList
+from task.py import Task
+from jiui.py import *
+
+while close():
+    gui()
+    pass
